19a0d360c346c4027f6a70821075efe4350c7f4c
秃尾巴的猫 <472647301@qq.com>
2021-09-27 16:42:51 +0800

    Update RCTRefreshLayout.java

--- android/src/main/java/com/byron/scroller/RCTRefreshLayout.java
+++ android/src/main/java/com/byron/scroller/RCTRefreshLayout.java
@@ -61,7 +61,7 @@ public class RCTRefreshLayout extends SmartRefreshLayout {
     public void addView(View child, int index) {
         if (child instanceof RCTRefreshHeader) {
 //             this.setRefreshHeader((RCTRefreshHeader) child);
-            this.setRefreshHeader((RCTRefreshHeader) child,0,300);
+            this.setRefreshHeader((RCTRefreshHeader) child,0,250);
         } else if (child instanceof ReactScrollView) {
             this.setRefreshContent(child);
         }
